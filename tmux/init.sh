19b48b21fc765895ca3839b6e3f4445fa2e5f5b6
Hayato <birdofnight123@gmail.com>
2024-01-16 02:01:14 +0900

    Tab -> Space

--- tmux/init.sh
+++ tmux/init.sh
@@ -5,12 +5,12 @@ DIR=$(cd $(dirname $0) && pwd)
 
 # tmux 設定ファイル配置場所を作成
 if [ ! -d $HOME/.config/tmux ]; then
-	mkdir -p $HOME/.config/tmux
+    mkdir -p $HOME/.config/tmux
 fi
 
 # 既存の tmux 設定ファイルのバックアップを取る
 if [ -f $HOME/.config/tmux/tmux.conf ]; then
-	mv $HOME/.config/tmux/tmux.conf $HOME/.config/tmux/tmux.conf.backup
+    mv $HOME/.config/tmux/tmux.conf $HOME/.config/tmux/tmux.conf.backup
 fi
 
 # tmux 設定ファイルをシンボリックリンクで配置
